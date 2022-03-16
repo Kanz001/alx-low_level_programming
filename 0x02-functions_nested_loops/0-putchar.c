d29175a267883a46719acdd3c93fb3de4c95d076
Kanz001 <abdulhameedabdulfatai@gmail.com>
2022-03-17 00:20:07 +0100

    newer

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+  * main - entry block
+  * Description - a function that prints "_putchar"
+  * * Return: 0;
+  */
+int main(void)
+{
+	char p[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
