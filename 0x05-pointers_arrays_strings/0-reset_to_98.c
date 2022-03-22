00681903f875fe09bad815c32acbfd0c3723f27d
Kanz001 <abdulhameedabdulfatai@gmail.com>
2022-03-22 05:00:37 +0100

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * reset_to_98 - update the value of n
+  *
+  * @n: value to be updated
+  *
+  * Return: no return value
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
