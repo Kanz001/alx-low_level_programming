87dd87506cdaf872235af95f1bdffb460792ab40
Kanz001 <abdulhameedabdulfatai@gmail.com>
2022-03-23 01:45:23 +0100

    quick fix

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * puts2 - print every character in a string with a newline
+  *
+  * @str: Input string
+  *
+  * Return: no return value
+  */
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (*(str + count) != '\0')
+	{
+		if (count % 2 == 0)
+		{
+			_putchar(*(str + count));
+		}
+		count++;
+	}
+	_putchar('\n');
+}
