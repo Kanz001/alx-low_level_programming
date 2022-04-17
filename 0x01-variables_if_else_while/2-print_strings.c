169b39d8d89598a58625d8e7adea14191ae7cb16
Kanz001 <abdulhameedabdulfatai@gmail.com>
2022-04-17 15:39:40 +0100

    Hot fix

---
+++ 0x01-variables_if_else_while/2-print_strings.c
@@ -0,0 +1,36 @@
+ #include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings, followed by a new line
+ *
+ * @separator: string to separate parameters
+ * @n: number of parameters
+ *
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *s;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(ap, char *);
+		if (s)
+			printf("%s", s);
+		else
+			printf("(nil)");
+		if (i < n - 1)
+			if (separator)
+				printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(ap);
+}
+
